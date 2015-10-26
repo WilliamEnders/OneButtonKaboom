6ecf0fad6c7503c633f114c3591f262fbcf2f7b8
William Enders <14hourlunchbreak@gmail.com>
2015-10-26 14:26:21 -0400

    Added X axis rotation

--- OneButtonKaboom/Assets/Scripts/planetSizer.cs
+++ OneButtonKaboom/Assets/Scripts/planetSizer.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class planetSizer : MonoBehaviour {
 
 	public float radius;
-	public float spinSpeed;
+	public float spinSpeedY;
+	public float spinSpeedX;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +17,7 @@ public class planetSizer : MonoBehaviour {
 	void Update(){
 		transform.localScale = new Vector3 (radius, radius, radius);
 
-		transform.Rotate (new Vector3(0,spinSpeed,0));
+		transform.Rotate (new Vector3(0,spinSpeedY,spinSpeedX));
 	}
 
 }
