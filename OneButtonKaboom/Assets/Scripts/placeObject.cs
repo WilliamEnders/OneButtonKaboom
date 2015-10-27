41513eb599acde8450b7b0e4477dd92c0085f06c
William Enders <14hourlunchbreak@gmail.com>
2015-10-27 17:11:35 -0400

    Added Pipes, Pixel Clouds, and SpawnerSpawners

--- OneButtonKaboom/Assets/Scripts/placeObject.cs
+++ OneButtonKaboom/Assets/Scripts/placeObject.cs
@@ -6,15 +6,10 @@ public class placeObject : MonoBehaviour {
 	private Transform planet;
 	public float offEarth;
 
-	// Use this for initialization
-	void Start () {
+	// Update is called once per frame
+	public void placeMe () {
 		planet = GameObject.FindGameObjectWithTag ("Planet").transform;
 		transform.position = new Vector3 (0,0+(planet.localScale.y / 2)+offEarth+(transform.localScale.y),0);
 		transform.parent.gameObject.transform.rotation = Random.rotation;
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
 	}
 }
