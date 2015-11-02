c0f3bf08c0830de086815757fcf188d57ff61833
William Enders <14hourlunchbreak@gmail.com>
2015-11-01 20:31:59 -0500

    added a bunch o stuff

---
+++ OneButtonKaboom/Assets/textureChanger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textureChanger : MonoBehaviour {
+
+	public Material[] materials;
+	int currentMat = 0;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material = materials[currentMat];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
