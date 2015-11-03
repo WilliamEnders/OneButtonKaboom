728cd7e72d80af66df911bf75749bbd4b9d70d4b
Li Yang <yangl395@newschool.edu>
2015-11-03 16:10:54 -0500

    Hell Wall added

---
+++ OneButtonKaboom/Assets/hell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hell : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnCollisionEnter(Collision collision) {
+		GameObject selectedObject = collision.gameObject;
+		selectedObject.GetComponent<item>().moveBack(); 
+	}
+}
